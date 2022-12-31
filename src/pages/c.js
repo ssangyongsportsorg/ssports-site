85aa3d3852b3d1b8e2141a425728acdc18fc4c11
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2022-12-31 22:31:25 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import contact from "../components/contact.js";
 
 export default function Hello() {
   return (
-    <Layout title="Hello" description="Hello React Page">
+    <Layout title="聯絡" description="聯絡雙龍體育">
       <div
         style={{
           display: 'flex',
@@ -13,116 +14,9 @@ export default function Hello() {
           fontSize: '20px',
         }}>
         <p>
-    <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-
- <section className="formcarry-container">
-  <form action="#" method="POST" encType="multipart/form-data">
-    <div className="formcarry-block">
-      <label htmlFor="fc-generated-1-name">Full Name</label>
-      <input
-        type="text"
-        name="name"
-        id="fc-generated-1-name"
-        placeholder="Your first and last name"
-      />
-    </div>
-    <div className="formcarry-block">
-      <label htmlFor="fc-generated-1-email">Your Email Address</label>
-      <input
-        type="email"
-        name="email"
-        id="fc-generated-1-email"
-        placeholder="john@doe.com"
-      />
-    </div>
-    <div className="formcarry-block">
-      <label htmlFor="fc-generated-1-message">Your message</label>
-      <textarea
-        name="message"
-        id="fc-generated-1-message"
-        placeholder="Enter your message..."
-        defaultValue={""}
-      />
-    </div>
-    <div className="formcarry-block">
-      <button type="submit">Send</button>
-    <>
-  <p>&nbsp;</p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-  <p>
-    <br />
-  </p>
-</>
-</div>
-  </form>
-</section>        
+       <contact />
 
 </p>
-  <link rel="stylesheet" href="https://ssangyongsports.github.io/1.css" />
 
       </div>
     </Layout>
