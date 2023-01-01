e04548759cba00336334dbde29feb412070eec0a
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-01 18:21:32 +0800

    Update index.tsx

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -7,27 +7,27 @@ export default function Newsletter(): JSX.Element {
     <section className={styles.root}>
       <div className={clsx("container", styles.wrapper)}>
         <div>
-          <h2 className={styles.heading}>Subscribe for product updates</h2>
+          <h2 className={styles.heading}>訂閱雙龍體育任何新消息</h2>
           <div className={styles.subheading}>
-            By subscribing, you agree with Revue’s{" "}
-            <a target="_blank" href="https://www.getrevue.co/terms">
-              Terms of Service
+            訂閱及表示你同意{" "}
+            <a target="_blank" href="/t">
+              服務條款
             </a>{" "}
             and{" "}
-            <a target="_blank" href="https://www.getrevue.co/privacy">
-              Privacy Policy
+            <a target="_blank" href="/p">
+              隱私政策
             </a>
             .
           </div>
         </div>
         <form
-          action="https://www.getrevue.co/profile/easypanel/add_subscriber"
+          action="https://ssangyongsports.us11.list-manage.com/subscribe/post?u=d2e34413aa6cbf7c94020e4ae&amp;id=6b0083dcd5&amp;f_id=00d796e0f0"
           method="post"
           target="_blank"
           className={styles.form}
         >
           <input
-            placeholder="Email address"
+            placeholder="電子郵件"
             type="email"
             name="member[email]"
             className={clsx(styles.input, "button")}
