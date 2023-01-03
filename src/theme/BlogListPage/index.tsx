7edfd6ca6f66234836e0ace26e6cd82f423ebd7a
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-03 19:34:07 +0800

    Update index.tsx

--- src/theme/BlogListPage/index.tsx
+++ src/theme/BlogListPage/index.tsx
@@ -1,65 +1,151 @@
-import React from "react";
-import clsx from "clsx";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import {
-    PageMetadata,
-    HtmlClassNameProvider,
-    ThemeClassNames,
-} from "@docusaurus/theme-common";
-import BlogLayout from "@theme/BlogLayout";
-import BlogListPaginator from "@theme/BlogListPaginator";
-import SearchMetadata from "@theme/SearchMetadata";
-import BlogPostItems from "@theme/BlogPostItems";
 
+import React from "react"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import Layout from "@theme/Layout"
+import BlogPostItem from "@theme/BlogPostItem"
+import BlogListPaginator from "@theme/BlogListPaginator"
+import type { FrontMatter as OriginalFrontMatter } from "@theme/BlogPostPage"
+import type { Props } from "@theme/BlogListPage"
+import type { Tag } from "@theme/BlogTagsListPage"
+import { ThemeClassNames } from "@docusaurus/theme-common"
 
-function BlogListPageMetadata(props) {
-    const { metadata } = props;
-    const {
-        siteConfig: { title: siteTitle },
-    } = useDocusaurusContext();
-    const { blogDescription, blogTitle, permalink } = metadata;
-    const isBlogOnlyMode = permalink === "/";
-    const title = isBlogOnlyMode ? siteTitle : blogTitle;
-    return (
-        <>
-            <PageMetadata title={title} description={blogDescription} />
-            <SearchMetadata tag="blog_posts_list" />
-        </>
-    );
+import styles from "./styles.module.css"
+import { ListItem } from "./ListItem"
+import { Categories } from "./Categories"
+import type { Props as CategoriesProps } from "./Categories"
+import { Chips } from "./Chips"
+import type { Props as ChipProps } from "./Chips"
+
+export type FrontMatter = OriginalFrontMatter & { permalink?: string }
+
+const categories: CategoriesProps["categories"] = [
+  {
+    title: "Benchmarks",
+    description:
+      "Reproducible benchmarks of QuestDB and other databases using open source benchmarking frameworks",
+    url: "/blog/tags/benchmark",
+  },
+  {
+    title: "Demos",
+    description:
+      "Demos involving QuestDB and other popular open source tools for a wide range of use cases",
+    url: "/blog/tags/demo",
+  },
+  {
+    title: "Tutorials",
+    description:
+      "Step-by-step tutorials and guides for developers to build applications with QuestDB",
+    url: "/blog/tags/tutorial",
+  },
+  {
+    title: "User Stories",
+    description:
+      "How QuestDB powers the core infrastructure of our users for time series data and real-time analytics",
+    url: "/customers",
+  },
+]
+
+const prioritizedTags: ChipProps["items"] = [
+  "release",
+  "engineering",
+  "python",
+  "grafana",
+  "crypto",
+  "sql",
+  "kafka",
+  "prometheus",
+  "telegraf",
+  "company",
+  "community",
+].map((tag) => ({
+  name: tag,
+  permalink: `/blog/tags/${tag.replace(/ /g, "-")}`,
+}))
+
+function BlogListPage(props: Props): JSX.Element {
+  const { metadata, items } = props
+  const {
+    siteConfig: { title: siteTitle },
+  } = useDocusaurusContext()
+  const { blogDescription, blogTitle, permalink } = metadata
+  const isBlogOnlyMode = permalink === "/"
+  const isTagsPage =
+    typeof ((metadata as unknown) as Tag).allTagsPath !== "undefined"
+  const title = isBlogOnlyMode ? siteTitle : blogTitle
+
+  const posts = [...items]
+  const latestPost = metadata.page === 1 ? posts.shift() : undefined
+
+  return (
+    <Layout
+      title={title}
+      description={blogDescription}
+      wrapperClassName={ThemeClassNames.wrapper.blogPages}
+      pageClassName={ThemeClassNames.page.blogListPage}
+      searchMetadatas={{
+        // assign unique search tag to exclude this page from search results!
+        tag: "blog_posts_list",
+      }}
+    >
+      <main className={styles.root}>
+        {latestPost !== undefined && (
+          <div className={styles.latestPost}>
+            <BlogPostItem
+              key={latestPost.content.metadata.permalink}
+              frontMatter={latestPost.content.frontMatter}
+              metadata={{
+                ...latestPost.content.metadata,
+                permalink:
+                  (latestPost.content.frontMatter as FrontMatter).permalink ??
+                  latestPost.content.metadata.permalink,
+                tags: [],
+              }}
+              truncated={latestPost.content.metadata.truncated}
+            >
+              {React.createElement(latestPost.content)}
+            </BlogPostItem>
+          </div>
+        )}
+
+        <h2>Popular topics</h2>
+        {/* BlogListPage component is used for blog and for tags.
+                When rendered for tags, then `metadata` includes tag, instead of blog data */}
+        <Categories
+          activeCategory={((metadata as unknown) as Tag).permalink}
+          categories={categories}
+        />
+        <Chips
+          activeChip={((metadata as unknown) as Tag).permalink}
+          items={prioritizedTags}
+        />
+
+        <h2>
+          {isTagsPage
+            ? `Articles tagged with "${((metadata as unknown) as Tag).name}"`
+            : "Blog Posts"}
+        </h2>
+
+        <div className={styles.posts}>
+          {posts.map(({ content }, i) => (
+            <ListItem
+              key={content.metadata.permalink}
+              content={content}
+              belowFold={i > 5}
+              forcedTag={
+                isTagsPage
+                  ? {
+                      label: ((metadata as unknown) as Tag).name,
+                      permalink: metadata.permalink,
+                    }
+                  : undefined
+              }
+            />
+          ))}
+
+          
+      </main>
+    </Layout>
+  )
 }
 
-function BlogListPageContent(props) {
-    const { metadata, items } = props;
-
-    const isFirstPage = metadata.page === 1;
-
-    const featuredPosts = items.filter(
-        (post) => post.content.metadata.frontMatter.is_featured === true,
-    );
-
-    const paginatedPosts = items.filter(
-        (post) => post.content.metadata.frontMatter.is_featured !== true,
-    );
-
-    return (
-        <BlogLayout>
-            <BlogPostItems items={paginatedPosts} />
-            <br />
-            <BlogListPaginator metadata={metadata} />
-        </BlogLayout>
-    );
-}
-
-export default function BlogListPage(props) {
-    return (
-        <HtmlClassNameProvider
-            className={clsx(
-                ThemeClassNames.wrapper.blogPages,
-                ThemeClassNames.page.blogListPage,
-            )}
-        >
-            <BlogListPageMetadata {...props} />
-            <BlogListPageContent {...props} />
-        </HtmlClassNameProvider>
-    );
-}
+export default BlogListPage
