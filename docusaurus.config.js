815bce628232eb151f74885daa4dc97838dc5337
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-04 22:37:40 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,16 +21,16 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'zh',
-    locales: ['zh'],
+   i18n: {
+    defaultLocale: 'zh-tw',
+    locales: ['zh-tw'],
   },
   plugins: [
     [
       require.resolve('@cmfcmf/docusaurus-search-local'),
       {
         indexPages: true,
-        language: "zh-tw",
+        language: "zh",
         
       },
     ],
