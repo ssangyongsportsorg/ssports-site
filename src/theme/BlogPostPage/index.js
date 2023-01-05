c450cc41ced7660769c12d5c08978304939c64eb
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-05 22:29:44 +0800

    Update index.js

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -1,67 +1,41 @@
+import React, { type ReactNode } from "react";
+import clsx from "clsx";
+import { HtmlClassNameProvider, ThemeClassNames } from "@docusaurus/theme-common";
+import { BlogPostProvider, useBlogPost } from "@docusaurus/theme-common/internal";
+import BlogLayout from "@theme/BlogLayout";
+import BlogPostItem from "@theme/BlogPostItem";
+import BlogPostPaginator from "@theme/BlogPostPaginator";
+import BlogPostPageMetadata from "@theme/BlogPostPage/Metadata";
+import TOC from "@theme/TOC";
+import type { Props } from "@theme/BlogPostPage";
+import type { BlogSidebar } from "@docusaurus/plugin-content-blog";
+
 import { ReactCusdis } from 'react-cusdis'
-import React from "react"
-import { ThemeClassNames } from "@docusaurus/theme-common"
-import BlogPostItem from "@theme/BlogPostItem"
-import type { Props } from "@theme/BlogPostPage"
-import BlogPostPaginator from "@theme/BlogPostPaginator"
-import BlogSidebar from "@theme/BlogSidebar"
-import EditThisPage from "@theme/EditThisPage"
-import Layout from "@theme/Layout"
-import Toc from "@theme/TOC"
-import { Section } from "../../components/Section"
-import { ActionFooter } from "../../components/ActionFooter"
 
-function BlogPostPage(props: Props): JSX.Element {
-  const { content: BlogPostContents, sidebar } = props
-  const { frontMatter, metadata } = BlogPostContents
-  const { title, description, nextItem, prevItem, editUrl } = metadata
-  const { hide_table_of_contents: hideTableOfContents } = frontMatter
-
+function BlogPostPageContent({ sidebar, children }: { sidebar: BlogSidebar; children: ReactNode }): JSX.Element {
+  const { metadata, toc } = useBlogPost();
+  const { nextItem, prevItem, frontMatter } = metadata;
+  const {
+    hide_table_of_contents: hideTableOfContents,
+    toc_min_heading_level: tocMinHeadingLevel,
+    toc_max_heading_level: tocMaxHeadingLevel,
+    slug,
+    title,
+    no_comments
+  } = frontMatter;
   return (
-    <Layout
-      title={title}
-      description={description}
-      wrapperClassName={ThemeClassNames.wrapper.blogPages}
-      pageClassName={ThemeClassNames.page.blogPostPage}
+    <BlogLayout
+      sidebar={sidebar}
+      toc={
+        !hideTableOfContents && toc.length > 0 ? (
+          <TOC toc={toc} minHeadingLevel={tocMinHeadingLevel} maxHeadingLevel={tocMaxHeadingLevel} />
+        ) : undefined
+      }
     >
-      {BlogPostContents != null && (
-        <div className="container margin-vert--lg">
-          <div className="row">
-            <div className="col col--3">
-              <BlogSidebar sidebar={sidebar} />
-            </div>
-            <main className="col col--7">
-              <BlogPostItem
-                frontMatter={frontMatter}
-                metadata={metadata}
-                isBlogPostPage
-              >
-                <BlogPostContents />
-              </BlogPostItem>
-              <div>
-                {typeof editUrl === "string" && (
-                  <EditThisPage editUrl={editUrl} />
-                )}
-              </div>
-              {(nextItem != null || prevItem != null) && (
-                <div className="margin-vert--xl">
-                  <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
-                </div>
-              )}
-            </main>
+      <BlogPostItem>{children}</BlogPostItem>
 
-            {typeof hideTableOfContents === "boolean" &&
-              !hideTableOfContents &&
-              BlogPostContents.toc && (
-                <div className="col col--2">
-                  <Toc toc={BlogPostContents.toc} />
-                </div>
-              )}
-          </div>
-        </div>
-      )}
-<div>
-      <ReactCusdis
+      {!no_comments && (
+        <ReactCusdis
         attrs={{
           host: 'https://dfff-nijxcc7sb-ssangyongsports.vercel.app',
           appId: '583956c6-98dd-40e3-9d48-7ee4480f3d28',
@@ -71,12 +45,23 @@ function BlogPostPage(props: Props): JSX.Element {
         }}
       />
 
-    </div>
-      <Section>
-        <ActionFooter />
-      </Section>
-    </Layout>
-  )
+      )}
+
+      {(nextItem || prevItem) && <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />}
+    </BlogLayout>
+  );
 }
 
-export default BlogPostPage
+export default function BlogPostPage(props: Props): JSX.Element {
+  const BlogPostContent = props.content;
+  return (
+    <BlogPostProvider content={props.content} isBlogPostPage>
+      <HtmlClassNameProvider className={clsx(ThemeClassNames.wrapper.blogPages, ThemeClassNames.page.blogPostPage)}>
+        <BlogPostPageMetadata />
+        <BlogPostPageContent sidebar={props.sidebar}>
+          <BlogPostContent />
+        </BlogPostPageContent>
+      </HtmlClassNameProvider>
+    </BlogPostProvider>
+  );
+}
