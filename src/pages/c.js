3b35f12d2d4614a833f4dd16516ce17f43d7ccae
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-01-23 23:32:41 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -58,25 +58,7 @@ function Form() {
 
               </p>
             </form>
-<div className="wc-sks-wrp hidden">
-  <div className="wc-sks-knt">
-    <svg
-      className="wc-sks-svg"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" />
-    </svg>
-    <h2>聯繫已成功發送</h2>
-    <p>
-      我們將在1-4天內回復您的郵件，定期檢查您的電子郵件收件匣，包括垃圾郵件箱.
-    </p>
-    <p>
-      {" "}
-      <a href="javascript:location.reload();">關閉視窗</a>
-    </p>
-  </div>
-</div>
+
 
           </div>
         </div>
