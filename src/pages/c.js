8422af8288413b201b37ce996a9b70f92770ec59
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-02-03 18:56:59 +0800

    Update c.js

--- src/pages/c.js
+++ src/pages/c.js
@@ -9,10 +9,13 @@ function Form() {
   return (
     <Layout title="聯絡">
       <main>
-          <h1>
+                <div className={styles.content}>
+  
+    <h1>
          聯繫雙龍體育團隊
           </h1>
-       
+                 </div>
+
                 <div className={styles.formwrapper}>
           <div className={styles.formbox}>
             <form
